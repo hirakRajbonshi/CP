871191d70a6c628c437a19220d66c302344d98dd
karock <148215838+hirakRajbonshi@users.noreply.github.com>
2026-04-10 23:39:55 +0530

    Implement Trie for MEX finding

--- Tree/trie.cpp
+++ Tree/trie.cpp
@@ -54,3 +54,91 @@ struct Trie {
         return res;
     }
 };
+
+
+// trie implementation for finding MEX
+template <typename node, size_t N, size_t B> 
+struct Trie {
+  vector<node> tree;
+  Trie() {
+    tree.clear();
+    tree.reserve(N * B);
+    tree.push_back(node());
+  }
+  void insert(int x) {
+    stack<int> st;
+    int cur = 0;
+    for (int i = B - 1; i >= 0; i--) {
+      int b = x >> i & 1;
+      if (!tree[cur].have(b)) {
+        tree[cur].add(b, tree.size());
+        tree.push_back(node());
+      }
+      cur = tree[cur].get(b);
+      st.push(cur);
+    }
+    st.pop();
+    tree[cur].present = true;
+    tree[cur].cnt++;
+    while (!st.empty()) {
+      cur = st.top(); st.pop();
+      tree[cur].cnt = 0;
+      for (int b = 0; b < 2; b++) 
+        if (tree[cur].have(b))
+            tree[cur].cnt += tree[tree[cur].get(b)].get_cnt();
+    }
+  }
+  void erase(int x) {
+    stack<int> st;
+    int cur = 0;
+    for (int i = B - 1; i >= 0; i--) {
+        int b = x >> i & 1;
+        assert(tree[cur].have(b));
+        cur = tree[cur].get(b);
+        st.push(cur);
+    }
+    st.pop();
+    tree[cur].cnt--;
+    if (!tree[cur].cnt) tree[cur].present = false;
+    while (!st.empty()) {
+        cur = st.top(); st.pop();
+        tree[cur].cnt = 0;
+        for (int b = 0; b < 2; b++) 
+        if (tree[cur].have(b))
+            tree[cur].cnt += tree[tree[cur].get(b)].get_cnt();
+    }
+  }
+  int find() {
+    int cur = 0, res = 0;
+    for (int i = B - 1; i >= 0; i--) {
+      bool ok = false;
+      if (tree[cur].have(0)) {
+        int cnt = tree[tree[cur].get(0)].get_cnt();
+        ok = cnt == (1 << i);
+      }
+      if (ok) {
+        res ^= (1 << i);
+        if (tree[cur].have(1)) cur = tree[cur].get(1);
+        else break;
+      } else {
+        if (tree[cur].have(0)) cur = tree[cur].get(0);
+        else break;
+      }
+    }
+    return res;
+  }
+};
+struct node {
+  int cnt;
+  bool present;
+  int children[2];
+  node() {
+    cnt = 0;
+    present = false;
+    children[0] = children[1] = -1;
+  }
+  bool have(int b) { return ~children[b]; }
+  int get(int b) { return children[b]; }
+  void add(int b, int idx) { children[b] = idx; }
+  int get_cnt() { return present ? 1 : cnt; }
+};
